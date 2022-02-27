077ec54a06db77f3b4fa68197aa15522f6b1482d
Kriksy <viktoria.toldi@gmail.com>
2022-02-27 22:13:09 +0100

    Add connecting to database

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
 
 app.use("/static", express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 
-//view engine configuration
+// connection to db
+// mongoose.set("useFindAndModify", false);
+
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
+    console.log("Connected to db");
+
+    app.listen(3000, () => console.log("Server Up and running on 3000"));
+})
+
+// view engine configuration
 app.set("view engine", "ejs");
 
 // GET METHOD
@@ -17,5 +30,3 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
     console.log(req.body);
 });
-
-app.listen(3000, () => console.log("Server Up and running on 3000"));
